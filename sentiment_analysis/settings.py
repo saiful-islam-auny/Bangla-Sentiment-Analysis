7b271bc9bcb8b868649868ddf04bf7a776c6528d
Saiful Islam Auny <aunychowdhury99@gmail.com>
2025-03-21 21:05:32 +0600

    setting and req upadted

--- sentiment_analysis/settings.py
+++ sentiment_analysis/settings.py
@@ -39,7 +39,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ["*"]
-# CSRF_TRUSTED_ORIGINS = ['','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://bangla-sentiment-analysis.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
